3c9d872ae2229611be30bf46633871d2ced2e33a
nicobolton2 <nicoboltongithub@gmail.com>
2024-05-21 20:41:29 +0200

    clase 5
    se realiza el enrutado general, agregando home ruta principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 
+export const routes : Routes=[
+  {
+    path:'',
+    loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule),
+  },{
+    path: '',
+    redirectTo : '/',
+    pathMatch: 'full',
+  },{
+    path: '**',
+    redirectTo : 'error/404',
+  }
 
+]
 
 @NgModule({
-  declarations: [],
+  // declarations: [],
   imports: [
-    CommonModule
-  ]
+    RouterModule.forRoot(routes),
+  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
