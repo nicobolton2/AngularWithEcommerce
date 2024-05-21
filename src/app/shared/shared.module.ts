772292d785904840b9bac6f36263b57f4fa710c1
nicobolton2 <nicoboltongithub@gmail.com>
2024-05-21 20:51:09 +0200

    Revert "clase 5"
    This reverts commit 3c9d872ae2229611be30bf46633871d2ced2e33a.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent
+  ],
   imports: [
     CommonModule
   ]
 })
-export class AppRoutingModule { }
+export class SharedModule { }
